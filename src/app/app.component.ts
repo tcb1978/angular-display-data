a29967cf31e82efeb217b0a32d8ca5d42296b243
Matthew Eldredge <mattheweldredge@gmail.com>
2017-06-21 20:28:59 -0700

    add stuff bc I am awesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,46 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `<h1>Welcome to {{title}}!!</h1>
+  <input type="text" value="Add To Do">
+  <button>Add</button>
+  <ul>
+    <li *ngFor="let todos of newTodo"></li>
+  </ul>
+  `
 })
 export class AppComponent {
-  title = 'app';
+    title = 'app';
+    newTodo: string;
+    todos: any;
+    todoObj: any;
+
+    constructor() {
+      this.newTodo = '';
+      this.todos = ['Take out the trash', 'Laundry', 'Groceries'];
+    }
+
+    addTodo(event) {
+      this.todoObj = {
+        newTodo: this.newTodo,
+        completed: false
+      }
+      this.todos.push(this.todoObj);
+      this.newTodo = '';
+      event.preventDefault();
+    }
+
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+    }
+
+    deleteSelectedTodos() {
+      //need ES5 to reverse loop in order to splice by index
+      for(var i=(this.todos.length -1); i > -1; i--) {
+        if(this.todos[i].completed) {
+          this.todos.splice(i, 1);
+        }
+      }
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+ 
+import { AppComponent }  from './app.component';
+import { ToDoComponent } from './to-do/to-do.component';
+ 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    ToDoComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
